8636829952907eb871aa1b79f67f8e30f3d3d57e
Eljoey <eljoeyy@gmail.com>
2020-11-15 12:03:50 -0600

    update cors

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => { console.log('Connected to BDO-Stuff Database'); });
 
 //Middleware
-app.use(cors());
+app.use(cors({ credentials: true, origin: ['http://localhost:3001', 'http://bdo-stuff.netlify.app/'] }));
 app.use(bodyparser.json());
 app.use(cookieParser());
 app.use(middleware.getToken);
